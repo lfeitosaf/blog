4eb91db21dd43daf4a7ca0027338377157f48c02
lfeitosaf <50138385+lfeitosaf@users.noreply.github.com>
2019-06-19 21:51:41 -0300

    Add files via upload

---
+++ dbfake.js
@@ -0,0 +1,41 @@
+var db = {
+    dados: [
+        {
+            titulo: 'Versão de Journey para PC ganha data de lançamento',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/Ei4k9F6ShmpapRkt8gnScQ-346x188.jpg',
+
+        }
+    ]
+
+
+}
+var dbb = {
+    dados: [
+        {
+            titulo: 'Confira todos os detalhes do rework do Mordekaiser no League of Legends',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/rework-do-Mordekaiser-no-League-of-Legends-346x188.jpg',
+        }
+    ]
+
+
+}
+var dbbb = {
+    dados: [
+        {
+            titulo: 'Listagens de lojas chinesas mostram The Witcher 3 para Nintendo Switch',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/The-Witcher-3-Nintendo-Switch-346x188.jpg',
+        }
+    ]
+
+
+}
+var dbbbb = {
+    dados: [
+        {
+            titulo: 'Rumor afirma que trailer e data de lançamento de The Last of Us Part II serão revelados esta semana',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/The-Last-of-Us-Part-II-346x188.jpg',
+        }
+    ]
+
+
+}
---
+++ dbfake2.js
@@ -0,0 +1,45 @@
+var db = {
+    dados: [
+        {
+            titulo: 'Versão de Journey para PC ganha data de lançamento',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/Ei4k9F6ShmpapRkt8gnScQ-346x188.jpg',
+            descrição: 'Depois da sua versão para PC ter sido anunciada em dezembro do ano passado, a desenvolvedora thatgamecompany junto com a publisher Annapurna Interactive, revelaram que Journey estará disponível na Epic Games Store a partir do dia 6 de junho. Além de todas as melhorias já feitas na versão de PS4, Journey será lançado para PC com suporte total a gráficos 4K. O game já está em pré-venda e será comercializado por US$ 15, mas durante a Epic Mega Sale você pode adquiri-lo por apenas US$ 6,99. Lançado originalmente em 2012 para PS3, e depois relançado em 2015 para PS4, Journey é um dos jogos mais aclamado do PlayStation tanto pela crítica como pelo público, tendo uma pontuação de 92/100 no Metacritic. Caso você nunca tenha ouvido falar do jogo, em Journey você controla um personagem encapuzado que vaga por uma vasta paisagem desértica, podendo encontrar e se “conectar” com outros jogadores durante a sua jornada.',
+
+        }
+    ]
+
+
+}
+var dbb = {
+    dados: [
+        {
+            titulo: 'Confira todos os detalhes do rework do Mordekaiser no League of Legends',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/rework-do-Mordekaiser-no-League-of-Legends-346x188.jpg',
+            descrição: 'Depois de uma longa espera, a Riot Games finalmente liberou nesta terça-feira (28) os principais detalhes do rework do Mordekaiser, que incluem uma mudança completa tanto no seu visual como no seu kit de habilidades. Mantendo o seu estilo de jogo como um colosso, Mordekaiser ainda é capaz de gerar poderosos escudos e causar grandes quantidade de dano, principalmente se estiver em uma lane solo. Começando pela sua passiva, a “Ascensão das Trevas” faz com que os ataques básicos de Mordekaiser causem Dano Mágico adicional. Depois de três ataques ou habilidades contra um campeão, a passiva causa dano aos inimigos próximos e oferece velocidade de movimento ataque que Mordekaiser esteja fora de combate. O seu Q é o “Obliterar”, que faz Mordekaiser bater com sua maça no chão causando dano em área ou dano adicional se utilizado em um único inimigo. O seu W é o “Indestrutível”, que basicamente é um escudo que absorver 25% do dano recebido e do dano causado, no melhor estilo Tahm Kench. Conjurar a habilidade novamente faz Mordekaiser consumir 50% do escudo em troca de vida. O seu E é o “Aperto Mortal”, que possui como habilidade passiva garantir 25% de penetração mágica e a sua habilidade ativa lança uma garra que causa dano e arrasta inimigos na sua direção. Por fim a sua ultimate é o “Reino da Morte”, que funciona de uma maneira nunca antes vista no jogo. Mordekaiser bane um campeão para o reino da morte e os dois lutarão por 7 segundos. Caso Mordekaiser consiga eliminar o seu adversário, ele ganhará atributos parciais até o inimigo renascer. Mordekaiser  chega em League of Legends na atualização 9.12.',
+        }
+    ]
+
+
+}
+var dbbb = {
+    dados: [
+        {
+            titulo: 'Listagens de lojas chinesas mostram The Witcher 3 para Nintendo Switch',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/The-Witcher-3-Nintendo-Switch-346x188.jpg',
+            descrição: 'Com a E3 2019 se aproximando mais vazamentos começam estão acontecendo, e o mais recente deles ocorreu novamente no fórum ResetEra, em que um usuário conhecido como Jim_Cacher publicou supostas imagens de uma versão de The Witcher 3: The Wild Hunt para o Nintendo Switch. De acordo com o usuário, as imagens foram encontradas no site chinês Taobao.com, que traz algumas listagens de grandes distribuidoras chinesas, algo que oferece um pouco mais de veracidade ao vazamento. Na publicação, o usuário ainda diz que uma “fonte confiável” afirmou que essa  versão Game of the Year de The Witcher 3 terá dublagem em chinês exclusiva para a Ásia e deve chegar em setembro. Como nada foi confirmado pela Nintendo, esse vazamentos ainda devem ser tratados como rumor. De qualquer forma, é interessante pensar em como The Witcher 3 rodaria no Nintendo Switch. Sabemos que até mesmo o PS4 e o Xbox One têm dificuldades em rodar o jogo da CD Projekt, então, para que isso aconteça no Switch basicamente existem duas opções viáveis. A primeira delas é que ele funcionará como Assassin’s Creed Odyssey e Resident Evil 7 no Japão, que rodam no Switch através da nuvem. A segunda e um pouco mais improvável é que The Witcher 3 será destinado especificamente a versão mais potente do Switch, que nessa caso seria equivalente em termos de hardware aos consoles base dessa atual geração. Mas só saberemos se isso realmente é real na apresentação da Nintendo na E3 2019, que será transmitida no dia 11 de junho às 13h (Horário de Brasília).',
+        }
+    ]
+
+
+}
+var dbbbb = {
+    dados: [
+        {
+            titulo: 'Rumor afirma que trailer e data de lançamento de The Last of Us Part II serão revelados esta semana',
+            imagem: 'https://criticalhits.com.br/wp-content/uploads/2019/05/The-Last-of-Us-Part-II-346x188.jpg',
+            descrição: 'Além de Death Stranding, que terá mais novidades reveladas nesta quarta-feira (29), parece que a Sony está preparando para esta semana outra surpresa de um dos seus exclusivos mais aguardados. De acordo com os sites espanhóis Gamereactor e Legion de Jugadores, o aguardado The Last of Us Part II receberá um novo trailer esta semana com uma data de lançamento oficial. Ambos os sites afirmam terem obtidos as informações com “fontes próximas” a Sony, mas nenhum deles confirmar detalhes adicionais ou diz quando exatamente esse trailer será lançado. Como a Sony não confirmou nada, a informação ainda deve ser tratada como um rumor. No entanto, existem alguns padrões que valem apena serem notados. O primeiro deles é que todos os vazamentos e rumores sobre possíveis datas de The Last of Us Part II, apontam que o game será lançado ainda em 2019, então, se isso realmente for acontecer, já estaria na hora da Sony fazer uma confirmação oficial. Além disso, também podemos especular que a Sony talvez esteja se preparando para realizar um grande State of Play no dia 29 de maio, com a revelação de trailer e data de lançamento de Death Stranding e The Last of Us Part II. Só estes dois jogos poderiam não ser o suficiente para fechar uma conferência na E3, mas sem dúvida seriam perfeitos para um evento realizado uma semana antes. Até o momento, sabemos pouquíssimos detalhes sobre The Last of Us Part II, mas a Naughty Dog já falou que o jogo não será totalmente linear e que você jogará toda a campanha principal com a Ellie, que será acompanhada por um NPC.',
+        }
+    ]
+
+
+}
